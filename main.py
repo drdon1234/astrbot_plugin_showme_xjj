65011a9274e88e13f7d6170e152ff7f4dafa6daa
drdon1234 <96719161+drdon1234@users.noreply.github.com>
2025-04-10 18:23:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import asyncio
 import json
 import logging
 
-upload_file(self, event: AstrMessageEvent, path: str, name: str = None, folder_name: str = '/')
-
 @register("astrbot_plugin_showme_xjj", "drdon1234", "随机小姐姐视频", "1.0", "https://github.com/drdon1234/astrbot_plugin_showme_xjj")
 class randomXJJPlugin(Star):
     def __init__(self, context: Context):
@@ -16,12 +14,14 @@ class randomXJJPlugin(Star):
         self.config = load_config()
         self.uploader = MessageAdapter(self.config)
 
-    async def get_random_media(self, media_type="video"):
+    async def get_random_media(self, event: AstrMessageEvent, media_type="video"):
         cache_folder = Path(self.config['download']['cache_folder'])
         cache_folder.mkdir(exist_ok=True, parents=True)
         if media_type == "video":
+            text = "视频"
             api_list = self.config["api"]["video_api"]
         elif media_type == "picture":
+            text = "图片"
             api_list = self.config["api"]["picture_api"]
         else:
             raise ValueError(f"不支持的媒体类型: {media_type}")
@@ -29,29 +29,19 @@ class randomXJJPlugin(Star):
         api_config = random.choice(api_list)
         cache_folder = self.config["download"]["cache_folder"]
         result = await get_url(api_config, cache_folder)
-    
-        return result
+        try:
+            await event.send(event.plain_result(f"xjj{text}正在赶来的路上，请接收..."))
+            await self.uploader.upload_file(event, event, video_url)
+        except Exception as e:
+            await event.send(event.plain_result(f"获取随机视频失败: {e}"))
     
     @filter.command("xjj视频")
     async def moyu_daily(self, event: AstrMessageEvent):
-        cache_folder = Path(self.config['download']['cache_folder'])
-        cache_folder.mkdir(exist_ok=True, parents=True)
-        try:
-            video_url = await get_random_media("video")
-            await event.send(event.plain_result("xjj视频正在赶来的路上，请接收..."))
-            await upload_file
-        except Exception as e:
-            await event.send(event.plain_result(f"获取随机视频失败: {e}"))
+        await self.get_random_media(event, "video")
 
     @filter.command("xjj图片")
     async def moyu_daily(self, event: AstrMessageEvent):
-        try:
-            picture_url = await get_random_media("picture")
-            await event.send(event.plain_result("xjj图片正在赶来的路上，请接收..."))
-        except Exception as e:
-            await event.send(event.plain_result(f"获取随机图片失败: {e}"))
+        await self.get_random_media(event, "picture")
 
-    
-    
     async def terminate(self):
         pass
