0b09be46f8c1b6e1aa1bf2f098353f106289ebd7
drdon1234 <96719161+drdon1234@users.noreply.github.com>
2025-04-10 19:20:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,11 @@ class randomXJJPlugin(Star):
         cache_folder.mkdir(exist_ok=True, parents=True)
         if media_type == "video":
             text = "视频"
+            ext = "mp4"
             api_list = self.config["api"]["video_api"]
         elif media_type == "picture":
             text = "图片"
+            ext = "jpg"
             api_list = self.config["api"]["picture_api"]
         else:
             raise ValueError(f"不支持的媒体类型: {media_type}")
@@ -33,7 +35,7 @@ class randomXJJPlugin(Star):
         result = await get_url(api_config, cache_folder)
         try:
             await event.send(event.plain_result(f"xjj{text}正在赶来的路上，请接收..."))
-            await self.uploader.upload_file(event, result, f"xjj{text}")
+            await self.uploader.upload_file(event, result, f"xjj{text}.{ext}")
         except Exception as e:
             await event.send(event.plain_result(f"获取随机视频失败: {e}"))
     
